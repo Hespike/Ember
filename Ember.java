bda427167e1d5e3afc623d74d7002d58fc94f8cf
Sárdi András <sardi.andras@gmail.com>
2021-04-11 13:38:18 +0200

    Create Ember.java

---
+++ Ember.java
@@ -0,0 +1,13 @@
+public class Ember {
+
+    void asit() {}
+    void alszik(int mennyit) {}
+    void eszik(Etel mit) {}
+    void iszik(Ital mit) {}
+    boolean okos() {return true;}
+    boolean ferfi() {return false;}
+    boolean vasarol(int[] ar) {return true;}
+    int szamol(int a, int b, int c) { return 0;}
+    String velemeny(int a, double b) {return "";}
+
+}
